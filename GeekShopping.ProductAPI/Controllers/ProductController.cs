4489154f24d059db5dbb472c99154cf0a3b214a6
Gustavo <gustavohs2004@gmail.com>
2022-06-16 18:22:22 -0300

    Criando controller de produto

---
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _repository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+        [HttpGet]
+        public async Task<IActionResult> FindAll()
+        {
+            var product = await _repository.FindAll();
+            if (product == null)
+                return NotFound();
+            return Ok(product);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product == null)
+                return NotFound();
+            return Ok(product);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Post([FromBody] ProductVO product)
+        {
+            if (product == null)
+                return BadRequest();
+            var productEntity = await _repository.Create(product);
+            return Ok(productEntity);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ProductVO>> Put([FromBody] ProductVO product)
+        {
+            if (product == null)
+                return BadRequest();
+            var productEntity = await _repository.Update(product);
+            return Ok(productEntity);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = await _repository.Delete(id);
+            if (!status)
+                return BadRequest();
+            return Ok(status);
+        }
+    }
+}
